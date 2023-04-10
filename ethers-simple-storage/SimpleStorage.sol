9dc12700db5b06ae3ece8d9e96dad77bbf68d94f
Juanma <jmbardaviov@bankinter.com>
2023-04-10 18:54:00 +0200

    first post!

---
+++ ethers-simple-storage/SimpleStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity ^0.8.8; //comment
+
+contract SimpleStorage {
+    uint256 favouriteNumber;
+
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    People[] public people;
+
+    function store(uint256 _favoriteNumber) public virtual {
+        favouriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favouriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favoriteNumber;
+    }
+}
---
+++ ethers-simple-storage/deploy.js
@@ -0,0 +1,6 @@
+async function main() {
+  console.log("hi");
+  let variable = 7;
+  console.log(variable);
+}
+main();
